13fcd20a44142f50d3d254a5207fec3343628129
yehonadav <yonadav.barilan@gmail.com>
2019-09-02 00:12:03 +0300

    build candidate build/2019.9.1.21.12.2.127916

--- qaviton_package_manager/__init__.py
+++ qaviton_package_manager/__init__.py
@@ -13,7 +13,7 @@
 
 
 __author__ = 'Qaviton'
-__version__ = "2019.9.1.19.40.56.494921"
+__version__ = "2019.9.1.21.12.2.127916"
 __author_email__ = 'info@qaviton.com'
 __description__ = 'a package manager for projects with private repositories'
 __url__ = 'https://github.com/qaviton/qaviton_package_manager'
