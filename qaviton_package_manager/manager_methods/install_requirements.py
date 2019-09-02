7270ecfb6a2727ae5601213e27aef1c61778e750
yehonadav <yonadav.barilan@gmail.com>
2019-09-02 03:43:18 +0300

    build candidate build/2019.9.2.0.43.18.6134

--- qaviton_package_manager/manager_methods/install_requirements.py
+++ qaviton_package_manager/manager_methods/install_requirements.py
@@ -46,7 +46,7 @@ class Install(ManagerOperation):
                     if package_match(package.replace(' ', ''), requirement):
                         requirements[i] = None
             with open(self.requirements_path, 'w') as f:
-                f.writelines([pkg for pkg in requirements if pkg is not None] + list(self.packages))
+                f.writelines([pkg for pkg in requirements if pkg is not None] + list({'\n'+pkg for pkg in self.packages}))
 
 
 class InstallTest(TestOperation, Install):
