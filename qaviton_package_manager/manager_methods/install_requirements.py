58dbb3379a5abc3c4eb0ad267b67d488425768cc
yehonadav <yonadav.barilan@gmail.com>
2019-09-20 16:48:22 +0300

    solving this: https://github.com/coala/coala-bears/issues/1161 https://github.com/pybuilder/pybuilder/issues/388

--- qaviton_package_manager/manager_methods/install_requirements.py
+++ qaviton_package_manager/manager_methods/install_requirements.py
@@ -21,56 +21,25 @@ from qaviton_package_manager.utils.functions import package_match
 from qaviton_package_manager.utils.logger import log
 from qaviton_processes import run
 from sys import executable
+from qaviton_git import Git
 
 
-class Install(ManagerOperation):
-    def run(self):
-        install_requirements = len(self.packages) == 0 or self.packages[0] is None
-        if install_requirements:
-            self.get_packages_from_requirements()
-
-        packages = ' '.join(self.configure_packages())
-
-        vcs_packages = {}
+class PackageManager:
+    def __init__(self, git: Git, packages: list):
+        self.git = git
+        self.pip_packages = packages
+        self.vcs_packages = {}
         for pkg in packages:
-            if 'git+' in pkg or '#egg=' in pkg:
-                error = ValueError(
-                    f"we detected an unsupported vcs installation:{pkg}\n"
-                    "please view the supported vcs formats of pip here:\n"
-                    "https://pip.pypa.io/en/stable/reference/pip_install/#vcs-support\n\n"
-                    "out of those, we dont support git://git.example.com/MyProject#egg=MyProject\n"
-                    "our format is as follows:\n"
-                    "git+protocol://git.example.com/MyProject.git@{branch}#egg=MyProject{:optional version tags}\n"
-                    # ':1.1.1,1.1.2,!1.1.3,1.1.*,!1.1.*,!1.1,>1.1,<1.1,1.1-1.3,>=1.1,<=1.1,!(1.2-1.3)'
-                    "we urge you to use https for a secure network transaction.")
-                if 'git+' not in pkg or '#egg=' not in pkg: raise error
-                try:
-                    a = pkg.split('git+', 1)
-                    b = a[1].rsplit('#egg=', 1)
-                    c = b[0].split('.git@', 1)
+            self.create_package_dict(pkg)
 
-                    if ':' in b[1]:
-                        name, version = b[1].split(':', 1)
-                        version = version.split(',')
-                    else:
-                        name, version = b[1], []
-
-                    vcs_packages[name] = {
-                        'link': pkg,
-                        'url': c[0] + '.git',
-                        'branch': c[1],
-                        'vcs': 'git',
-                        'protocol': a[1].split(':', 1)[0],
-                        'version': version}
-                except: raise error
-
-        if vcs_packages:
-            packages = [pkg for pkg in packages if 'git+' not in pkg]
-            installed = pip('freeze').decode('utf-8').splitlines()
+        if self.vcs_packages:
+            self.pip_packages = [pkg for pkg in packages if 'git+' not in pkg]
+            self.installed = pip('freeze').decode('utf-8').splitlines()
             with TemporaryDirectory() as tmp:
-                for name, pkg in vcs_packages.items():
+                for name, pkg in self.vcs_packages.items():
                     for i in installed:
-                        if i.split('==')[0] == name.replace('_', '-'):
+                        ins, ver = i.split('==')
+                        if ins == name.replace('_', '-'):
                             # lines = r.splitlines()
                             # version = None
                             # for line in lines:
@@ -79,12 +48,12 @@ class Install(ManagerOperation):
                             #         version = line[len(v):]
                             #         break
                             #
-                            log.info(f'{name} from {pkg["link"]} requirement already satisfied')
+                            log.info(f'{i} requirement from {pkg["link"]} already satisfied')
                             break
                     else:
                         pkg['path'] = tmp+sep+name
                         repo = self.git.clone(
-                            path=pkg['path'] ,
+                            path=pkg['path'],
                             url=pkg['url'],
                             username=self.git.username,
                             password=self.git.password,
@@ -97,6 +66,13 @@ class Install(ManagerOperation):
                             version = pkg['versions'][-1]
                         else:
                             version = pkg['versions'][-1]
+                        try:
+                            with open(pkg['path']+sep+'requirements.txt', encoding='utf-8') as f:
+                                r = f.read().splitlines()
+                        except FileNotFoundError as e:
+                            log.error(e)
+                            log.error(f"{pkg['name']} from {pkg['link']} is missing requirements.txt file")
+                            raise FileNotFoundError("")
                         run('cd', pkg['path'], '&&', executable, 'setup.py bdist_wheel --universal')
                         dist = pkg['path'] + sep + 'dist'
                         for fn in listdir(dist):
@@ -105,8 +81,66 @@ class Install(ManagerOperation):
                                 print(get_metadata(wheel).requires_dist)
                                 break
 
+    def get_dependency_tree(self, pkg: dict):
+        tree = []
+        while pkg['parent']:
+            tree.append(pkg['parent'])
+            pkg = self.vcs_packages[pkg['parent']]
+        return tree
+
+    def create_package_dict(self, uri: str, **kwargs):
+        if 'git+' in uri or '#egg=' in uri:
+            error = ValueError(
+                f"we detected an unsupported vcs installation:{uri}\n"
+                "please view the supported vcs formats of pip here:\n"
+                "https://pip.pypa.io/en/stable/reference/pip_install/#vcs-support\n\n"
+                "out of those, we dont support git://git.example.com/MyProject#egg=MyProject\n"
+                "our format is as follows:\n"
+                "git+protocol://git.example.com/MyProject.git@{branch}#egg=MyProject{:optional version tags}\n"
+                # ':==1.1.1,1.1.2,!1.1.3,1.1.*,!1.1.*,!1.1,>1.1,<1.1,1.1-1.3,>=1.1,<=1.1,!(1.2-1.3)'
+                "we urge you to use https for a secure network transaction.")
+            if 'git+' not in uri or '#egg=' not in uri: raise error
+            try:
+                a = uri.split('git+', 1)
+                b = a[1].rsplit('#egg=', 1)
+                c = b[0].split('.git@', 1)
+
+                if ':' in b[1]:
+                    name, version = b[1].split(':', 1)
+                    version = version.split(',')
+                else:
+                    name, version = b[1], []
+
+                pkg = {
+                    'link': uri,
+                    'url': c[0] + '.git',
+                    'branch': c[1],
+                    'vcs': 'git',
+                    'protocol': a[1].split(':', 1)[0],
+                    'version': version,
+                    'versions': [],
+                    'name': name,
+                    'parent': None,
+
+                }
+                pkg.update(kwargs)
+                self.vcs_packages[name] = pkg
+            except:
+                raise error
+            return pkg
+
+
+class Install(ManagerOperation):
+    def run(self):
+        install_requirements = len(self.packages) == 0 or self.packages[0] is None
+        if install_requirements:
+            self.get_packages_from_requirements()
+
+        packages = self.configure_packages()
+        manager = PackageManager(self.git, packages)
+
         if packages:
-            pip.install(packages)
+            pip.install(manager.pip_packages)
 
             # TODO: fix this, add check for version evaluation
             # if not install_requirements and self.packages:
