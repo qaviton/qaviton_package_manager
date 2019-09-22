cc53315c2dd7c1a0b3fd7e4e933aff5514a73276
yehonadav <yonadav.barilan@gmail.com>
2019-09-22 16:17:55 +0300

    build candidate dev/2019.9.22.13.17.55.30885

--- ci_cd.py
+++ ci_cd.py
@@ -9,15 +9,16 @@ if __name__ == "__main__":
 
     manager.run(
         # lambda: manager.should_build(from_branch=build_branch, to_branch=release_branch),
-        lambda: manager.update(),
-        lambda: manager.update_test(),
+        # lambda: manager.update(),
+        # lambda: manager.update_test(),
         # lambda: manager.test(),
-        lambda: manager.build(release_branch),
-        lambda: manager.upload(),
-        lambda: git.push(),
-        lambda: git.switch('master'),
-        lambda: git.pull(),
-        lambda: git(f'rebase {build_branch}'),
-        lambda: git.push(),
-        lambda: git.switch(working_branch),
+        manager.build(to_branch='dev/latest')
+        # lambda: manager.build(release_branch),
+        # lambda: manager.upload(),
+        # lambda: git.push(),
+        # lambda: git.switch('master'),
+        # lambda: git.pull(),
+        # lambda: git(f'rebase {build_branch}'),
+        # lambda: git.push(),
+        # lambda: git.switch(working_branch),
     )
