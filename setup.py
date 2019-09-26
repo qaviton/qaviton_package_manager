5ea37ca9f4f2efb8f52a2fa2948c294416e08306
yehonadav <yonadav.barilan@gmail.com>
2019-09-26 15:29:17 +0300

    build candidate 2019.9.26.12.29.17.520891

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     with open("README.md", encoding="utf8") as f: long_description = f.read()
     setup(
         name=package_name,
-        version="2019.9.25.21.4.15.642951",
+        version="2019.9.26.12.29.17.520891",
         author="Qaviton",
         author_email="info@qaviton.com",
         description="a package manager for git projects with private repositories",
@@ -39,7 +39,6 @@ if __name__ == "__main__":
         install_requires=requirements,
         python_requires='!=2.*,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
     )
-
 
 
 
